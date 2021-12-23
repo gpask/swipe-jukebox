8ced76785ddf8fdb7c3db82bfcd95636f42bbf41
gpask <65040738+gpask@users.noreply.github.com>
2021-12-23 11:13:56 -0500

    Update cards.sh

--- cards.sh
+++ cards.sh
@@ -2,5 +2,5 @@
 
 for i in $(seq -f "%03g" 1 250)
 do
-	echo -e "2019.12.22    21:03:23\n%$i?\n;$i?\n+$i?\n" | tee -a cards.txt
+	echo -e "2021.12.23    11:13:23\n%$i?\n;$i?\n+$i?\n" | tee -a cards.txt
 done
